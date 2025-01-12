023ac5b144a21b73a9dd8dbfad28e420a0b939c0
jackjohn7 <branchjohnchristopher@gmail.com>
2025-01-12 03:12:27 -0600

    trying to export all

--- build.rs
+++ build.rs
@@ -1,4 +1,21 @@
+#[cfg(any(not(all(feature = "client", feature = "server")), feature = "all"))]
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure().compile_protos(&["protobuf/basalt.proto"], &["protobuf"])?;
+    Ok(())
+}
+#[cfg(feature = "client")]
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tonic_build::configure()
+        .build_client(true)
+        .build_server(false)
+        .compile_protos(&["protobuf/basalt.proto"], &["protobuf"])?;
+    Ok(())
+}
+#[cfg(feature = "server")]
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tonic_build::configure()
+        .build_client(false)
+        .build_server(true)
+        .compile_protos(&["protobuf/basalt.proto"], &["protobuf"])?;
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,1 @@
-mod protos {
-    tonic::include_proto!("basalt");
-}
-
-#[cfg(feature = "client")]
-pub mod client {
-    pub use crate::protos::auth_client;
-}
-
-#[cfg(feature = "server")]
-pub mod server {
-    pub use crate::protos::auth_server;
-}
+tonic::include_proto!("basalt");
